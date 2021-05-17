a34d416b3b4dcee89c0aea90b94f3e9c8c6a87b4
GENETEC\skatragadda <skatragadda@genetec.com>
2021-05-17 10:37:17 +0200

    Deep learning examples (main)

---
+++ DeepLearning/main.cpp
@@ -0,0 +1,19 @@
+// Created By: Sandeep Katragadda
+// https://github.com/ksandeep4u/CUDA-examples
+
+#include <iostream>
+#include "CoordinateClassification/coordinateClassification.h"
+#include "DigitClassification/digitClassification.h"
+
+using namespace std;
+
+int main() {
+	cout << "Building and training my first neural network using CUDA" << endl;
+	bool useGPU = false;
+
+	classifyCoordinates(useGPU);
+	//classifyDigits(useGPU);
+
+	return 0;
+}
+
